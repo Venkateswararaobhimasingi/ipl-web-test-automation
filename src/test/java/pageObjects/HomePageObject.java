d685bfe58f2db24cf2acd0573aad5d4067dd83f8
immidisettisaiganesh <saiganeshimmidisetti03@gmail.com>
2026-03-22 13:38:18 +0530

    Updated automation scripts / added new changes

--- src/test/java/pageObjects/HomePageObject.java
+++ src/test/java/pageObjects/HomePageObject.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 import base.BasePageObject;
 import utilities.WaitUtilities;
@@ -17,45 +18,77 @@ public class HomePageObject extends BasePageObject {
 		super(driver);
 	}
 
-	@FindBy(xpath = "//div[@class='site-navbar-wrap']//a[@data-element_text='TEAMS']") WebElement navBarTeamsLink;
-	
-	@FindBy(xpath = "//div[@class='site-navbar-wrap']//a[@data-element_text='POINTS TABLE']") List<WebElement> navBarPointsTableLink;
-	
-	
-	@FindBy(xpath = "//div[@class='site-navbar-wrap']//a[@data-element_text='MATCHES']") WebElement navBarMatchesLink;
-	
-	@FindBy(xpath = "//div[contains(@class,'matches-tabs')]//a[contains(text(),'POINTS TABLE')]") WebElement matchesNavBarPointsTableLink;
-	
+	@FindBy(xpath = "//div[@class='site-navbar-wrap']//a[@data-element_text='TEAMS']")
+	WebElement navBarTeamsLink;
+
+	@FindBy(xpath = "//div[@class='site-navbar-wrap']//a[@data-element_text='POINTS TABLE']")
+	List<WebElement> navBarPointsTableLink;
+
+	@FindBy(xpath = "//div[@class='site-navbar-wrap']//a[@data-element_text='MATCHES']")
+	WebElement navBarMatchesLink;
+
+	@FindBy(xpath = "//div[contains(@class,'matches-tabs')]//a[contains(text(),'POINTS TABLE')]")
+	WebElement matchesNavBarPointsTableLink;
+
 	By cookieBtn = By.xpath("//button[normalize-space()='Accept cookies']");
-	
-	
-	
+
+	@FindBy(xpath = "//button[@class='search-icon-header-menu bg-transparent border-0']")
+	WebElement btn_searchBar;
+
+	public void clickSearchBar() {
+		btn_searchBar.click();
+	}
+
+	@FindBy(xpath = "//select[@id='search_type']")
+	WebElement select_SearchType;
+
+	public void searchTypeNews() {
+		Select select = new Select(select_SearchType);
+		select.selectByVisibleText("News");
+	}
+
+	@FindBy(xpath = "//input[@id='searchInputForHeader']")
+	WebElement txt_search;
+
+	public void search(String text) {
+		txt_search.sendKeys(text);
+	}
+
+	@FindBy(xpath = "//b[@id='searchText']")
+	WebElement btn_search;
+
+	public void click_search() {
+		btn_search.click();
+	}
+
+	@FindBy(xpath = "//li[@class='textTwoLine']")
+	WebElement txt_searchResults;
+
+	public String searchResult() {
+		return txt_searchResults.getText();
+	}
 
 	public void navigateToPointsTable() {
-		
-		boolean isPointsTablePresent=navBarPointsTableLink.isEmpty();
-		
-		if(!isPointsTablePresent) {
-			
+
+		boolean isPointsTablePresent = navBarPointsTableLink.isEmpty();
+
+		if (!isPointsTablePresent) {
+
 			navBarPointsTableLink.get(0).click();
-		}
-		else {
-			
+		} else {
+
 			navBarMatchesLink.click();
-			
+
 			matchesNavBarPointsTableLink.click();
-			
+
 		}
-		
+
 	}
-	
+
 	public void navigateToTeams() {
-		
+
 		navBarTeamsLink.click();
 	}
-	
-	
-	
 
 	public void scrollToBottom() {
 
@@ -68,7 +101,7 @@ public class HomePageObject extends BasePageObject {
 
 		WebElement sectionTitle = WaitUtilities.waitForElementPresence(wait, sectionTitleLocator);
 
-		//js.executeScript("arguments[0].scrollIntoView(true);", sectionTitle);
+		// js.executeScript("arguments[0].scrollIntoView(true);", sectionTitle);
 
 		sectionTitle = WaitUtilities.waitForElementVisible(wait, sectionTitleLocator);
 
@@ -89,19 +122,17 @@ public class HomePageObject extends BasePageObject {
 		return sectionLinksText;
 
 	}
-	
+
 	public void handleCookies() {
 
 		List<WebElement> btns = driver.findElements(cookieBtn);
 
 		if (!btns.isEmpty() && btns.get(0).isDisplayed()) {
-		    btns.get(0).click();
-		    
+			btns.get(0).click();
+
 		} else {
-		    System.out.println("Cookie not present or already accepted");
+			System.out.println("Cookie not present or already accepted");
 		}
 	}
-
-	
 
 }
---
+++ src/test/java/testCases/TC_004_SearchFunctionality.java
@@ -0,0 +1,25 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pageObjects.HomePageObject;
+
+public class TC_004_SearchFunctionality extends BaseTest {
+
+	@Test
+	public void search() {
+
+		HomePageObject hm = new HomePageObject(driver);
+		
+		hm.clickSearchBar();
+		hm.search("Auction 2026");
+		hm.searchTypeNews();
+		hm.click_search();
+		
+		String res=hm.searchResult();
+		
+		Assert.assertEquals("TATA IPL 2026 Player Auction List Announced", res);
+	}
+}
