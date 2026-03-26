105f6b666841a50883f4b260a170e98f89058ad0
immidisettisaiganesh <saiganeshimmidisetti03@gmail.com>
2026-03-26 20:09:08 +0530

    added logger to tc 3 and 4

--- src/test/java/testCases/TC003_PointTableValidation.java
+++ src/test/java/testCases/TC003_PointTableValidation.java
@@ -11,20 +11,33 @@ public class TC003_PointTableValidation extends base.BaseTest {
 	@Test
 	public void testTeamNameAtRank1() {
 		
+		logger.info( "========Test Poinsts Table Validation Started=========");
 		hpo.navigateToPointsTable();
-
+		
+		logger.info("navigated to poinsts table");
 		PointsTablePageObject pto = new PointsTablePageObject(driver);
-
+		
+		logger.info("created object of Points table PageObject and redirected");
 		System.out.println(pto.getTeamRank1());
-
+		
+		logger.info("got the team ar rank1");
 		int played = pto.getMatchPlayed();
+		logger.info("got the matched played by team");
+		
 		int win = pto.getMatchWin();
+		logger.info("got the matched won by team");
+		
 		int lost = pto.getMatchLost();
+		logger.info("got the matched lost by team");
+
 		int tied = pto.getMatchTied();
+		logger.info("got the matched tied by team");
 
 		int points = pto.getMatchPoints();
+		logger.info("Calculated the actual points to a team and the present points to the team");
 
 		Assert.assertEquals(2 * win + tied == points, win + lost + tied == played);
+		logger.info("==Points table validation is compleated===");
 	}
 
 }
--- src/test/java/testCases/TC004_SearchFunctionality.java
+++ src/test/java/testCases/TC004_SearchFunctionality.java
@@ -11,15 +11,28 @@ public class TC004_SearchFunctionality extends BaseTest {
 	@Test
 	public void search() {
 
+		logger.info("===Testcase SearchFunctionality is started====");
+				
 		HomePageObject hm = new HomePageObject(driver);
-		
+		logger.info("Homepage Object is created sucessfully");
+
 		hm.clickSearchBar();
+		logger.info("cicked on searchBar");
+
 		hm.search("Auction 2026");
+		logger.info("searched the 'Auction2026 in search bar' ");
+
 		hm.searchTypeNews();
+		logger.info("selected the search type to news");
+
 		hm.click_search();
+		logger.info("clicked on search button ");
 		
 		String res=hm.searchResult();
+		logger.info("checked the search result from the search results");
 		
 		Assert.assertEquals("TATA IPL 2026 Player Auction List Announced", res);
+		logger.info("testcase searchFuntionality is compleated");
+		
 	}
 }
